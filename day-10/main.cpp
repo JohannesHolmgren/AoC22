81f769c49873c4db49b5839187701f8fb320943c
Johannes Holmgren <johholmg@student.chalmers.se>
2023-12-10 21:02:26 +0100

    old changes

--- day-10/main.cpp
+++ day-10/main.cpp
@@ -63,8 +63,20 @@ vector<pair<string, int>> getInstructions(vector<string> lines)
     return instructions;
 }
 
+void drawPixel(int cycle, int regX, int spriteWidth, char screen[6][40], int screenWidth)
+{
+    int pixelToDraw = cycle - 1;
+    int row = pixelToDraw / screenWidth;
+    int col = pixelToDraw % screenWidth;
+    char charToDraw = '.';
+    if ((pixelToDraw % screenWidth) <= regX + (spriteWidth / 2) && (pixelToDraw % screenWidth) >= regX - (spriteWidth / 2))
+        charToDraw = '#';
+    // cout << "pixel: " << pixelToDraw << " regX: " << regX << " char: " << charToDraw << endl;
+    screen[row][col] = charToDraw;
+}
+
 /* Part 1 */
-int runProgram(const vector<pair<string, int>> instructions)
+int runProgram(const vector<pair<string, int>> instructions, char screen[6][40])
 {
     // Runs the instructions and calculates the signal strength
     // for the 20th, 60th, 100th, 140th, 180th and 220th clock cycle
@@ -89,6 +101,10 @@ int runProgram(const vector<pair<string, int>> instructions)
     int cycle = 0;
     int regX = 1;
 
+    // For part 2: pixels
+    const int spriteWidth = 3;
+    const int screenWidth = 40;
+
     for (int i = 0; i < instructions.size(); i++)
     {
         pair<string, int> instr = instructions[i];
@@ -97,16 +113,19 @@ int runProgram(const vector<pair<string, int>> instructions)
             for (int step = 0; step < instructionCycles["noop"]; step++)
             {
                 cycle++;
+                // Draw pixel on screen
+                drawPixel(cycle, regX, spriteWidth, screen, screenWidth);
+                // check if read time (part 1)
                 if (isReadTime(cycle))
                 {
                     sumSignalStrengths += cycle * regX;
                     cout << "sumSignalStrength: " << sumSignalStrengths << " at cycle " << cycle << " with a = " << regX << endl;
                 }
-                // If program is much larger than 220, another signal strength could be counted
+                /* // If program is much larger than 220, another signal strength could be counted
                 else if (cycle > MAX_CYCLES)
                 {
                     return sumSignalStrengths;
-                }
+                } */
             }
         }
         else if (instr.first == "addx")
@@ -115,16 +134,19 @@ int runProgram(const vector<pair<string, int>> instructions)
             for (int i = 0; i < instructionCycles[instr.first]; i++)
             {
                 cycle++;
+                // Draw pixel on screen
+                drawPixel(cycle, regX, spriteWidth, screen, screenWidth);
+                // check if read time (part 1)
                 if (isReadTime(cycle))
                 {
                     sumSignalStrengths += cycle * regX;
                     cout << "sumSignalStrength: " << sumSignalStrengths << " at cycle " << cycle << " with X = " << regX << endl;
                 }
-                // If program is much larger than 220, another signal strength could be counted
+                /* // If program is much larger than 220, another signal strength could be counted
                 else if (cycle > MAX_CYCLES)
                 {
                     return sumSignalStrengths;
-                }
+                } */
             }
             regX += instr.second;
         }
@@ -136,9 +158,23 @@ int runProgram(const vector<pair<string, int>> instructions)
 int main()
 {
     const string FILENAME = "data.txt";
+
+    const int screenWidth = 40;
+    const int screenHeight = 6;
+    char screen[screenHeight][screenWidth];
+
     vector<string> data = readData(FILENAME);
     vector<pair<string, int>> instructions = getInstructions(data);
-    int signalStrength = runProgram(instructions);
+    int signalStrength = runProgram(instructions, screen);
     cout << "Part 1: " << signalStrength << endl;
+    cout << "Part 2: " << endl;
+    for (int row = 0; row < screenHeight; row++)
+    {
+        for (int col = 0; col < screenWidth; col++)
+        {
+            cout << screen[row][col];
+        }
+        cout << endl;
+    }
     return 0;
 }
